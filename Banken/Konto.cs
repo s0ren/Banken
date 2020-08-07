8e8484b223ee9ccdc484c76a178c024e83ebf66b
Søren Magnusson <smag@tec.dk>
2020-08-07 10:21:18 +0200

    Add project files.

---
+++ Banken/Konto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    public class Konto
+    {
+        public Konto(double startSaldo)
+        {
+            saldo = startSaldo;
+        }
+
+        public double saldo { get; private set; }
+
+        public void Indsæt(double beløb)
+        {
+            this.saldo += beløb;
+        }
+
+        public void Hæv(double beløb)
+        {
+            if (saldo >= beløb)
+            {
+                saldo -= beløb;
+            }
+        }
+
+    }
+}
